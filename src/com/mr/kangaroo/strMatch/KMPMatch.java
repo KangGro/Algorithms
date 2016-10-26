42d29c0eef38addfce9a6fabc1c2515cdda4c1cb
admin <admin@mooc1.chaoxing.com>
2016-10-26 18:24:00 +0800

    KMP next数组 部分匹配表

--- src/com/mr/kangaroo/strMatch/KMPMatch.java
+++ src/com/mr/kangaroo/strMatch/KMPMatch.java
@@ -16,6 +16,10 @@ public class KMPMatch {
 	
 	/**
 	 * 根據模式串生成部分匹配表;
+	 * "部分匹配值"就是"前缀"和"后缀"的最长的共有元素的长度;
+	 * 根据部分匹配值来确定移动的字符数， 已匹配的字符数 - 对应的部分匹配表中的值;
+	 * ABCDABD
+	 * 0000120
 	 * @param pattern
 	 * @return
 	 */
@@ -27,15 +31,18 @@ public class KMPMatch {
 				next[i] = 0;
 			}else{
 				int val = 0;
-				int j = 0,k = i - 1;
-				while(j < i && k >=0){
-					if(pattern.substring(0, j+1).equals(pattern.substring(k))){
-						val++;
+				int j = 0;
+				while(j < i){
+					String prefix = pattern.substring(0, j + 1);
+					String sufix = pattern.substring(i -j,i+1);
+					if(prefix.equals(sufix)){
+						if(prefix.length() > val){
+							val = prefix.length();
+						}
 					}
+					j++;
 				}
 				next[i] = val;
-				j++;
-				k--;
 			}
 		}
 		return next;
